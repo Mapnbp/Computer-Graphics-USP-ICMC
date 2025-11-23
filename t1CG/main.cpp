4601f83fd3d66f24072d07e3588645d43bbe7d8a
Mapnbp <matheusaraujopinh@gmail.com>
2025-11-22 21:59:47 -0300

    meio que todo o projeto até agora antes de eu experimente fazer algo e faça cagadas enormes

--- t1CG/main.cpp
+++ t1CG/main.cpp
@@ -1,245 +1,266 @@
 /**
- * @file main_modular.cpp
- * @brief Sistema de Preenchimento de Polígonos - Versão Modular
- * @brief Implementa o algoritmo de preenchimento ET/AET com arquitetura modular
- * @author Sistema de Preenchimento ET/AET
- * @date 2025
+ * @file main.cpp
+ * @brief Sistema de Computação Gráfica - Versão Integrada com UI
  */
 
-#include "auxcode/data_structures.h"
-#include "auxcode/polygon_manager.h"
-#include "auxcode/graphics_renderer.h"
-#include "auxcode/event_handler.h"
-#include "auxcode/polygon_fill_algorithm.h"
-#include <windows.h>
+#include <GL/glut.h>
 #include <iostream>
+#include <vector>
+#include <cmath>
 
-HWND mainWindowHandle;
-PolygonManager polygonManager;
-GraphicsRenderer* graphicsRenderer;
-EventHandler* eventHandler;
-ApplicationState currentApplicationState;
-WindowDimensions* currentWindowDimensions;
+// Includes dos módulos (usando 'core')
+#include "core/data_structures.h"
+#include "core/polygon_manager.h"
+#include "core/graphics_renderer.h"
+#include "core/event_handler.h"
+#include "core/scene_manager.h"
+#include "core/application_context.h"
 
-/**
- * @brief Callback da janela para processamento de mensagens do Windows
- * @param windowHandle Handle da janela
- * @param message Mensagem recebida
- * @param wParam Parâmetro W da mensagem
- * @param lParam Parâmetro L da mensagem
- * @return Resultado do processamento da mensagem
- */
-LRESULT CALLBACK WindowProcedure(HWND windowHandle, UINT message, WPARAM wParam, LPARAM lParam) {
-    switch (message) {
-        case WM_PAINT: {
-            PAINTSTRUCT paintStructure;
-            HDC deviceContext = BeginPaint(windowHandle, &paintStructure);
-            
-            RECT clientRect;
-            GetClientRect(windowHandle, &clientRect);
-            int width = clientRect.right - clientRect.left;
-            int height = clientRect.bottom - clientRect.top;
-            
-            HDC memoryDC = CreateCompatibleDC(deviceContext);
-            HBITMAP memoryBitmap = CreateCompatibleBitmap(deviceContext, width, height);
-            HBITMAP oldBitmap = (HBITMAP)SelectObject(memoryDC, memoryBitmap);
-            
-            HBRUSH backgroundBrush = CreateSolidBrush(RGB(0, 0, 0));
-            FillRect(memoryDC, &clientRect, backgroundBrush);
-            DeleteObject(backgroundBrush);
-            
-            eventHandler->handlePaintEvent(windowHandle, memoryDC);
-            
-            BitBlt(deviceContext, paintStructure.rcPaint.left, paintStructure.rcPaint.top,
-                paintStructure.rcPaint.right - paintStructure.rcPaint.left,
-                paintStructure.rcPaint.bottom - paintStructure.rcPaint.top,
-                memoryDC, paintStructure.rcPaint.left, paintStructure.rcPaint.top, SRCCOPY);
-            
-            SelectObject(memoryDC, oldBitmap);
-            DeleteObject(memoryBitmap);
-            DeleteDC(memoryDC);
-            
-            EndPaint(windowHandle, &paintStructure);
-            return 0;
+// Variáveis Globais de Shader
+PFNGLCREATESHADERPROC glCreateShader = NULL;
+PFNGLSHADERSOURCEPROC glShaderSource = NULL;
+PFNGLCOMPILESHADERPROC glCompileShader = NULL;
+PFNGLGETSHADERIVPROC glGetShaderiv = NULL;
+PFNGLGETSHADERINFOLOGPROC glGetShaderInfoLog = NULL;
+PFNGLCREATEPROGRAMPROC glCreateProgram = NULL;
+PFNGLATTACHSHADERPROC glAttachShader = NULL;
+PFNGLLINKPROGRAMPROC glLinkProgram = NULL;
+PFNGLGETPROGRAMIVPROC glGetProgramiv = NULL;
+PFNGLGETPROGRAMINFOLOGPROC glGetProgramInfoLog = NULL;
+PFNGLUSEPROGRAMPROC glUseProgram = NULL;
+PFNGLGETUNIFORMLOCATIONPROC glGetUniformLocation = NULL;
+PFNGLUNIFORM1FPROC glUniform1f = NULL;
+PFNGLUNIFORM3FPROC glUniform3f = NULL;
+
+// --- CALLBACKS GLUT ---
+
+void display() {
+    auto* app = ApplicationContext::getInstance();
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    if (app->currentMode == AppMode::MODE_2D_EDITOR) {
+        // --- MODO 2D ---
+        glMatrixMode(GL_PROJECTION);
+        glLoadIdentity();
+        if (app->windowDimensions) {
+            glOrtho(0, app->windowDimensions->width, app->windowDimensions->height, 0, -1, 1);
         }
-            
-        case WM_LBUTTONDOWN: {
-            int mouseX = LOWORD(lParam);
-            int mouseY = HIWORD(lParam);
-            eventHandler->handleMouseClick(mouseX, mouseY, false);
-            return 0;
+        glMatrixMode(GL_MODELVIEW);
+        glLoadIdentity();
+        glDisable(GL_DEPTH_TEST);
+        glDisable(GL_LIGHTING);
+
+        // Renderiza polígonos
+        app->graphicsRenderer.renderSavedPolygons(app->polygonManager.getSavedPolygons(), 
+                                                  app->windowDimensions->height, 
+                                                  app->windowDimensions->width);
+        
+        app->graphicsRenderer.renderPolygon(app->polygonManager.getVertices(), 
+                                            app->polygonManager.getVisualConfiguration(), 
+                                            app->polygonManager.isPolygonCurrentlyClosed());
+        
+        if (app->polygonManager.canBeFilled() && app->applicationState == ApplicationState::POLYGON_FILLED) {
+             app->graphicsRenderer.fillPolygon(app->polygonManager.getVertices(), 
+                                               app->polygonManager.getCurrentFillColor(), 
+                                               app->windowDimensions->height, 
+                                               app->windowDimensions->width);
         }
-            
-        case WM_RBUTTONDOWN: {
-            int mouseX = LOWORD(lParam);
-            int mouseY = HIWORD(lParam);
-            eventHandler->handleMouseClick(mouseX, mouseY, true);
-            return 0;
+        
+        app->graphicsRenderer.renderPolygonVertices(app->polygonManager.getVertices(), 
+                                                    app->polygonManager.getVisualConfiguration().showVertices);
+        
+        // === RENDERIZA UI NO MODO 2D ===
+        app->uiManager.render();
+
+    } else {
+        // --- MODO 3D ---
+        int w = glutGet(GLUT_WINDOW_WIDTH);
+        int h = glutGet(GLUT_WINDOW_HEIGHT);
+        
+        glEnable(GL_DEPTH_TEST);
+        glEnable(GL_LIGHTING);
+        app->sceneManager.updateProjectionMatrix(w, h);
+        app->sceneManager.render();
+
+        // --- Renderizar UI Overlay no modo 3D ---
+        glPushAttrib(GL_ALL_ATTRIB_BITS);
+        glDisable(GL_DEPTH_TEST);
+        glDisable(GL_LIGHTING);
+        
+        glMatrixMode(GL_PROJECTION);
+        glPushMatrix();
+        glLoadIdentity();
+        glOrtho(0, w, h, 0, -1, 1);
+        
+        glMatrixMode(GL_MODELVIEW);
+        glPushMatrix();
+        glLoadIdentity();
+        
+        app->uiManager.render();
+        
+        glMatrixMode(GL_PROJECTION);
+        glPopMatrix();
+        glMatrixMode(GL_MODELVIEW);
+        glPopMatrix();
+        glPopAttrib();
+    }
+
+    glutSwapBuffers();
+}
+
+void reshape(int w, int h) {
+    auto* app = ApplicationContext::getInstance();
+    
+    if (app->windowDimensions) delete app->windowDimensions;
+    app->windowDimensions = new WindowDimensions(w, h);
+    
+    if (app->eventHandler) {
+        app->eventHandler->updateWindowDimensions(app->windowDimensions);
+    }
+    
+    app->uiManager.updateLayout(w, h);
+    glViewport(0, 0, w, h);
+    glutPostRedisplay();
+}
+
+void keyboard(unsigned char key, int x, int y) {
+    auto* app = ApplicationContext::getInstance();
+    
+    if (key == 27) exit(0);
+    
+    if (key == 'm' || key == 'M') {
+        if (app->currentMode == AppMode::MODE_2D_EDITOR) {
+            app->create3DObjectsFrom2D();
+            app->currentMode = AppMode::MODE_3D_VIEWER;
+            std::cout << "--> Modo: Visualizador 3D" << std::endl;
+        } else {
+            app->currentMode = AppMode::MODE_2D_EDITOR;
+            std::cout << "--> Modo: Editor 2D" << std::endl;
         }
-            
-        case WM_MOUSEMOVE: {
-            int mouseX = LOWORD(lParam);
-            int mouseY = HIWORD(lParam);
-            eventHandler->handleMouseMove(mouseX, mouseY);
-            
-            eventHandler->updateMouseCursor(mouseX, mouseY);
-            return 0;
+        glutPostRedisplay();
+        return;
+    }
+
+    if (app->currentMode == AppMode::MODE_2D_EDITOR) {
+        if (app->eventHandler) app->eventHandler->handleKeyboardInput(key);
+    } else {
+        // Controles 3D: WASD QE para câmera
+        float speed = 0.5f;
+        Vector3D camPos = app->sceneManager.getCameraPosition();
+        
+        switch (key) {
+            case 'w': case 'W': camPos.z -= speed; break;  // Frente
+            case 's': case 'S': camPos.z += speed; break;  // Trás
+            case 'a': case 'A': camPos.x -= speed; break;  // Esquerda
+            case 'd': case 'D': camPos.x += speed; break;  // Direita
+            case 'q': case 'Q': camPos.y += speed; break;  // Cima
+            case 'e': case 'E': camPos.y -= speed; break;  // Baixo
+            case '1': app->sceneManager.setLightingModel(LightingModel::FLAT); break;
+            case '2': app->sceneManager.setLightingModel(LightingModel::GOURAUD); break;
+            case '3': app->sceneManager.setLightingModel(LightingModel::PHONG); break;
+            case 'p': case 'P': app->sceneManager.setProjection(ProjectionType::PERSPECTIVE); break;
+            case 'o': case 'O': app->sceneManager.setProjection(ProjectionType::ORTHOGRAPHIC); break;
         }
-            
-        case WM_KEYDOWN: {
-            int keyCode = static_cast<int>(wParam);
-            
-            if (keyCode == VK_ADD || keyCode == VK_OEM_PLUS) {
-                eventHandler->handleKeyboardInput('+');
-            } else if (keyCode == VK_SUBTRACT || keyCode == VK_OEM_MINUS) {
-                eventHandler->handleKeyboardInput('-');
+        app->sceneManager.setCameraPosition(camPos.x, camPos.y, camPos.z);
+    }
+    glutPostRedisplay();
+}
+
+void mouse(int button, int state, int x, int y) {
+    auto* app = ApplicationContext::getInstance();
+    
+    if (state == GLUT_DOWN) {
+        if (button == GLUT_LEFT_BUTTON) {
+            // Prioridade: UI consome cliques em ambos os modos
+            if (app->uiManager.handleClick(x, y)) {
+                glutPostRedisplay();
+                return;
+            }
+            // Se UI não consumiu, processa normalmente
+            if (app->eventHandler) app->eventHandler->handleMouseClick(x, y, false);
+        } else if (button == GLUT_RIGHT_BUTTON) {
+            if (app->currentMode == AppMode::MODE_2D_EDITOR) {
+                if (app->eventHandler) app->eventHandler->handleMouseClick(x, y, true);
             } else {
-                char keyPressed = static_cast<char>(wParam);
-                eventHandler->handleKeyboardInput(keyPressed);
+                app->isRightMouseButtonPressed = true;
+                app->lastMouseX = x;
+                app->lastMouseY = y;
             }
-            return 0;
         }
-            
-        case WM_SIZE: {
-            int newWidth = LOWORD(lParam);
-            int newHeight = HIWORD(lParam);
-            
-            if (currentWindowDimensions) {
-                delete currentWindowDimensions;
-            }
-            currentWindowDimensions = new WindowDimensions(newWidth, newHeight);
-            
-            if (eventHandler) {
-                eventHandler->updateWindowDimensions(currentWindowDimensions);
-            }
-            
-            InvalidateRect(windowHandle, NULL, TRUE);
-            return 0;
-        }
-            
-        case WM_DESTROY: {
-            PostQuitMessage(0);
-            return 0;
+    } else if (state == GLUT_UP) {
+        if (button == GLUT_RIGHT_BUTTON) {
+            app->isRightMouseButtonPressed = false;
+        } else if (button == GLUT_LEFT_BUTTON) {
+            app->uiManager.releaseAll();
         }
     }
-    
-    return DefWindowProc(windowHandle, message, wParam, lParam);
+    glutPostRedisplay();
 }
 
-/**
- * @brief Inicializa o sistema e cria a janela principal
- * @return true se a inicialização foi bem-sucedida, false caso contrário
- */
-bool initializeApplication() {
-    WNDCLASS windowClass = {};
-    windowClass.lpfnWndProc = WindowProcedure;
-    windowClass.hInstance = GetModuleHandle(NULL);
-    windowClass.lpszClassName = "PolygonFillSystem";
-    windowClass.hbrBackground = (HBRUSH)(COLOR_BTNFACE + 1);
-    windowClass.hCursor = LoadCursor(NULL, IDC_ARROW);
-    windowClass.style = CS_HREDRAW | CS_VREDRAW;
+void motion(int x, int y) {
+    auto* app = ApplicationContext::getInstance();
     
-    if (!RegisterClass(&windowClass)) {
-        std::cerr << "Erro ao registrar classe da janela" << std::endl;
-        return false;
+    if (app->currentMode == AppMode::MODE_3D_VIEWER) {
+        if (app->isRightMouseButtonPressed) {
+            int dx = x - app->lastMouseX;
+            int dy = y - app->lastMouseY;
+            
+            Vector3D lightPos = app->sceneManager.getLightPosition();
+            lightPos.x += dx * 0.1f;
+            lightPos.y -= dy * 0.1f;
+            app->sceneManager.setLightPosition(lightPos.x, lightPos.y, lightPos.z);
+            
+            app->lastMouseX = x;
+            app->lastMouseY = y;
+        }
     }
-    
-    mainWindowHandle = CreateWindowEx(
-        0,
-        "PolygonFillSystem",
-        "Sistema de Preenchimento de Poligonos - ET/AET",
-        WS_OVERLAPPEDWINDOW | WS_SIZEBOX,
-        CW_USEDEFAULT, CW_USEDEFAULT,
-        WINDOW_WIDTH, WINDOW_HEIGHT,
-        NULL, NULL,
-        GetModuleHandle(NULL), NULL
-    );
-    
-    if (mainWindowHandle == NULL) {
-        std::cerr << "Erro ao criar janela principal" << std::endl;
-        return false;
-    }
-    
-    currentApplicationState = ApplicationState::DRAWING_POLYGON;
-    
-    currentWindowDimensions = new WindowDimensions(WINDOW_WIDTH, WINDOW_HEIGHT);
-    
-    HDC deviceContext = GetDC(mainWindowHandle);
-    graphicsRenderer = new GraphicsRenderer(deviceContext, mainWindowHandle);
-    
-    eventHandler = new EventHandler(&polygonManager, graphicsRenderer, &currentApplicationState, currentWindowDimensions);
-    
-    return true;
+    glutPostRedisplay();
 }
 
-/**
- * @brief Exibe as instruções de uso do sistema
- */
-void displayUsageInstructions() {
-    std::cout << "================================================" << std::endl;
-    std::cout << "Sistema de Preenchimento de Polígonos - ET/AET" << std::endl;
-    std::cout << "================================================" << std::endl;
-    std::cout << std::endl;
-    std::cout << "Instruções de uso:" << std::endl;
-    std::cout << "- Clique esquerdo: Adicionar vértice" << std::endl;
-    std::cout << "- Clique direito: Fechar polígono" << std::endl;
-    std::cout << "- F: Fechar polígono" << std::endl;
-    std::cout << "- C: Limpar polígono" << std::endl;
-    std::cout << "- P: Preencher polígono" << std::endl;
-    std::cout << "- V: Alternar visibilidade dos vértices" << std::endl;
-    std::cout << "- +/-: Alterar espessura da linha" << std::endl;
-    std::cout << "- 1-6: Alterar cor do preenchimento" << std::endl;
-    std::cout << std::endl;
-    std::cout << "Cores disponíveis:" << std::endl;
-    std::cout << "1 - Vermelho    2 - Verde      3 - Azul" << std::endl;
-    std::cout << "4 - Amarelo     5 - Magenta    6 - Ciano" << std::endl;
-    std::cout << std::endl;
+void passiveMotion(int x, int y) {
+    auto* app = ApplicationContext::getInstance();
+    
+    if (app->currentMode == AppMode::MODE_2D_EDITOR) {
+        if (app->eventHandler) app->eventHandler->updateMouseCursor(x, y);
+    } else {
+        app->uiManager.handleHover(x, y);
+    }
+    glutPostRedisplay();
 }
 
-/**
- * @brief Limpa recursos alocados dinamicamente
- */
-void cleanupResources() {
-    if (eventHandler) {
-        delete eventHandler;
-        eventHandler = nullptr;
-    }
-    
-    if (graphicsRenderer) {
-        delete graphicsRenderer;
-        graphicsRenderer = nullptr;
-    }
-    
-    if (currentWindowDimensions) {
-        delete currentWindowDimensions;
-        currentWindowDimensions = nullptr;
-    }
-}
-
-/**
- * @brief Função principal do sistema
- * @param argc Número de argumentos da linha de comando
- * @param argv Argumentos da linha de comando
- * @return Código de saída do programa
- */
 int main(int argc, char** argv) {
-    if (!initializeApplication()) {
-        std::cerr << "Falha na inicialização do sistema" << std::endl;
-        return 1;
-    }
-    
-    displayUsageInstructions();
-    
-    ShowWindow(mainWindowHandle, SW_SHOW);
-    UpdateWindow(mainWindowHandle);
-    
-    MSG windowMessage = {};
-    while (GetMessage(&windowMessage, NULL, 0, 0)) {
-        TranslateMessage(&windowMessage);
-        DispatchMessage(&windowMessage);
-    }
-    
-    cleanupResources();
-    
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+    glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+    glutCreateWindow("Sistema de Computacao Grafica - OpenGL + GLUT");
+
+    ApplicationContext::getInstance()->init();
+
+    glutDisplayFunc(display);
+    glutReshapeFunc(reshape);
+    glutKeyboardFunc(keyboard);
+    glutMouseFunc(mouse);
+    glutMotionFunc(motion);
+    glutPassiveMotionFunc(passiveMotion);
+
+    std::cout << "========================================" << std::endl;
+    std::cout << "Sistema Iniciado - Modo 2D Editor" << std::endl;
+    std::cout << "========================================" << std::endl;
+    std::cout << "Controles:" << std::endl;
+    std::cout << "  M - Alternar 2D/3D" << std::endl;
+    std::cout << "  ESC - Sair" << std::endl;
+    std::cout << "Modo 2D:" << std::endl;
+    std::cout << "  Click - Adicionar vertice" << std::endl;
+    std::cout << "  F - Fechar poligono" << std::endl;
+    std::cout << "  P - Preencher" << std::endl;
+    std::cout << "  S - Salvar poligono" << std::endl;
+    std::cout << "Modo 3D:" << std::endl;
+    std::cout << "  WASD QE - Mover camera" << std::endl;
+    std::cout << "  1/2/3 - Flat/Gouraud/Phong" << std::endl;
+    std::cout << "  P/O - Perspectiva/Ortografica" << std::endl;
+    std::cout << "  Arrastar botao direito - Mover luz" << std::endl;
+    std::cout << "========================================" << std::endl;
+
+    glutMainLoop();
     return 0;
 }
